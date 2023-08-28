a715e906d9a766894e225a072492ae0718d59cd9
mainarel <64072803+mainarel@users.noreply.github.com>
2023-08-28 22:23:32 +0300

    Create 1.1_First_SQL_queries.sql

---
+++ SQL_basics/1_Simple_SQL_queries/1.1_First_SQL_queries.sql
@@ -0,0 +1,13 @@
+#1.1
+
+SELECT * FROM products;
+
+SELECT name, price FROM products;
+
+SELECT * FROM products WHERE price < 3000;
+
+SELECT name, price FROM products WHERE price >=10000;
+
+SELECT name FROM products WHERE count = 0;
+
+SELECT name, price FROM products WHERE price <=4000;
