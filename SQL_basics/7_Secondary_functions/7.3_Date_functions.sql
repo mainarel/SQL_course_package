53d393794f8d84f6e6380f93ddfb44ead5be29b3
mainarel <64072803+mainarel@users.noreply.github.com>
2023-10-12 15:57:00 +0300

    Update 7.3_Date_functions.sql

--- SQL_basics/7_Secondary_functions/7.3_Date_functions.sql
+++ SQL_basics/7_Secondary_functions/7.3_Date_functions.sql
@@ -16,15 +16,26 @@ select * from visits where
     order by date DESC;
 
 #7.3.4
-
+update calendar set visit_date = date_add(visit_date, interval "1:30" HOUR_MINUTE)
+where DATE(visit_date) = '2017-05-14' and hour(visit_date) >= 13;
   
 #7.3.5
-
+select user_id, DATE_FORMAT(date_add(date, interval "3" HOUR), '%d.%m.%Y %H:%i') as visit_date from visits
+order by date;
   
 #7.3.6
-
+select last_name, first_name, birthday from drivers 
+where sex = 'm' and timestampdiff(year, birthday,'2018-08-07')>=18
+order by last_name, first_name;
   
 #7.3.7
+select id, user_id, amount, date_format(date,'%d.%m.%Y %H:%i') as date from payments
+where DATE like '2017-03%'
+order by payments.date DESC
+limit 5;
 
-  
 #7.3.8
+select * from users where
+weekday(birthday) in ('5','6')
+order by birthday DESC;
+
